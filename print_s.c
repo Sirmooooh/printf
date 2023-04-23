b9e876f9fe3e8155ada0d286bfbd8d4570393265
Muhammad <sirmoohfabrics@gmail.com>
2023-04-23 03:40:59 -0700

    print_s.c

---
+++ print_s.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * printf_s - prints an hexgecimal number.
+ * @num: number to print.
+ * Return: counter.
+ */
+int printf_s(unsigned int num)
+{
+	int p, *array, counter = 0;
+	unsigned int temp = num;
+
+	for(counter = 1; num / 16 != 0; counter++)
+	{
+		num /= 16;
+	}
+	counter++;
+
+	array = malloc(counter * sizeof(int));
+	p = 0;
+	while (p < counter)
+	{
+		array[p] = temp % 16;
+		temp /= 16;
+		p++;
+	}
+	p = counter - 1;
+	while (p >= 0)
+	{
+		if (array[p] > 9)
+			array[p] = array[p] + 7;
+		_putchar(array[p] + '0');
+		p--;
+	}
+	free(array);
+	return (counter);
+}
