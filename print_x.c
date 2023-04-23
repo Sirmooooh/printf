d06c97e8ebf215a56668b755387de9d810855aeb
arly-p0 <ttaarlette@gmail.com>
2023-04-23 17:39:03 +0000

    print_x.c

---
+++ print_x.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_x - function that prints a str in reverse
+ * @args: type struct va_arg where is allocated printf arguments
+ *
+ * Return: the string
+ */
+int print_x(va_list args)
+{
+	char *s = va_arg(args, char*);
+	int a, b = 0;
+
+	if (!s)
+		s = "(null)";
+	while (s[b] != '\0')
+		b++;
+	for (a = b - 1; a >= 0; a--)
+		_putchar(s[i]);
+	return (b);
+}
