1d02a8f40114b911d872119a70499d495d78bca0
arly-p0 <ttaarlette@gmail.com>
2023-04-23 17:39:03 +0000

    print_pointer.c

---
+++ print_pointer.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_pointer - prints an hexgecimal number.
+ * @val: arguments.
+ * Return: counter.
+ */
+int print_pointer(va_list val)
+{
+	void *p;
+	char *s = "(nil)";
+	long int a;
+	int z, y;
+
+	p = va_arg(val, void*);
+	if (!p)
+	{
+		for (y = 0; s[y] != '\0'; y++)
+		{
+			_putchar(s[y]);
+		}
+		return (y);
+	}
+	a = (unsigned long int)p;
+	_putchar('0');
+	_putchar('x');
+	b = print_s(a);
+	return (z + 2);
+}
