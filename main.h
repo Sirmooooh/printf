a59972bf080476f41e0e29ea3aea9880d0a77962
Muhammad <sirmoohfabrics@gmail.com>
2023-04-23 02:47:25 -0700

    prinf.c

---
+++ main.h
@@ -0,0 +1,48 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+/**
+
+    struct format - match the conversion specifiers for printf
+    @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+    @f: type pointer to function for the conversion specifier
+
+*/
+
+typedef struct format
+{
+char *id;
+int (*f)();
+} convert_match;
+
+/* function prototypes */
+int _printf(const char *format, ...);
+int print_c(va_list args);
+int print_s(va_list args);
+int print_percent(va_list args);
+int print_i(va_list args);
+int print_d(va_list args);
+int print_b(va_list args);
+int print_u(va_list args);
+int print_o(va_list args);
+int print_x(va_list args);
+int print_X(va_list args);
+int print_S(va_list args);
+int print_p(va_list args);
+int _strlen(char *s);
+int *_strcpy(char *dest, char *src);
+int _putchar(char c);
+int print_37(void);
+int _strlenc(const char *s);
+int print_string(va_list val);
+int print_char(va_list val);
+int rev_string(char *s);
+int print_pointer(va_list val);
+
+#endif /* MAIN_H */
---
+++ printf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: lenght
+ */
+int _printf(const char * const format, ...)
+{
+	convert_match m[] = {
+		{"%s", print_string}, {"%c", print_char},
+		{"%%", print_37},
+		{"%i", print_S}, {"%d", print_p}, {"%r", print_x},
+		{"%R", print_X}, {"%b", print_o}, {"%u", print_u},
+		{"%o", print_b}, {"%x", print_d}, {"%X", print_i},
+		{"%S", print_percent}, {"%p", print_pointer}
+	};
+
+	va_list args;
+	int x = 0, y, len = 0;
+
+	va_start(args, format);
+	if (!format || (*format == '%' && !*(format + 1)))
+		return (-1);
+
+Here:
+	while (format[x] != '\0')
+	{
+		y = 13;
+		while (y >= 0)
+		{
+			if (m[y].id[0] == format[x] && m[y].id[1] == format[x + 1])
+			{
+				len += m[y].f(args);
+				x = x + 2;
+				goto Here;
+			}
+			y--;
+		}
+		_putchar(format[x]);
+		len++;
+		x++;
+	}
+	va_end(args);
+	return (len);
+}
