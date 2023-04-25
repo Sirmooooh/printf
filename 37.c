a66b220e2d3c17d0c0bdaa4e48ef1ff91d22123b
Muhammad <sirmoohfabrics@gmail.com>
2023-04-25 09:16:01 -0700

    char to print

--- 37.c
+++ 37.c
@@ -1,8 +1,14 @@
 #include "main.h"
+
 /**
- * printf_37 - prints the char 37.
- * Return: 1.
+ * print_37 - prints the character 37 using _putchar
+ *
+ * This function prints the character '37' using the _putchar function.
+ * It takes no arguments and returns the value 1.
+ *
+ * Return: 1
  */
+
 int print_37(void)
 {
 	_putchar(37);
